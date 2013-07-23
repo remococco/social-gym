a6282716f84bfa66e0e83545650fae5ea41d1f94
Stephen Cirner <scirner22@gmail.com>
2013-07-22 22:40:48 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialGym::Application.config.secret_key_base = '865031fcb8800469f9088593e1249f0df6eac667cda48ace7f6bff2e9af9d8eedcb11eaf7644e1f0c81c07c630fefd077b91f9641da7397d991ae316d61e1109'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SocialGym::Application.config.session_store :cookie_store, key: '_social-gym_session'
